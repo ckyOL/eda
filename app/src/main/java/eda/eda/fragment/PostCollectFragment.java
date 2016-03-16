80afc3c2812e8a6de3916c5b89194d62165b7c8d
piekey1994 <xiaoshendidi@163.com>
2016-03-16 22:25:53 +0800

    postfrag page

---
+++ app/src/main/java/eda/eda/fragment/PostCollectFragment.java
@@ -0,0 +1,77 @@
+package eda.eda.fragment;
+
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import eda.eda.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PostCollectFragment extends Fragment {
+
+
+
+
+    Uri uri;
+    ImageView image;
+    EditText brandText;
+    EditText kindText;
+    EditText styleText;
+    Button postButton;
+
+    private boolean isEmpty(String str)
+    {
+        return false;
+    }
+
+    public PostCollectFragment() {
+        // Required empty public constructor
+    }
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View root = inflater.inflate(R.layout.fragment_post_collect, container, false);
+        image=(ImageView) root.findViewById(R.id.userPic2);
+        brandText=(EditText) root.findViewById(R.id.brand);
+        kindText=(EditText) root.findViewById(R.id.kind);
+        styleText=(EditText) root.findViewById(R.id.style);
+        postButton=(Button) root.findViewById(R.id.postButton);
+        image.setImageURI(uri);
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isEmpty(brandText.getText().toString()))
+                {
+                    //弹窗
+                }
+                else if(isEmpty(kindText.getText().toString()))
+                {
+
+                    //弹窗
+                }
+                else if(isEmpty(styleText.getText().toString()))
+                {
+
+                    //弹窗
+                }
+                else
+                {
+                    //新建json连接
+                }
+            }
+        });
+        return root;
+    }
+
+}
